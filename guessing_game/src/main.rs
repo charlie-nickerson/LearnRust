db7af12fb9d37d218a1e9acd95979734ef7a1074
charlie-nickerson <charlie@nickerson.com>
2025-02-21 10:48:08 -0800

    Completed the first half of 2.1

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+
+
+fn main() {
+    println!("Guess The Number");
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // Mutable
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");   
+
+    println!("You guessed {}", guess);
+
+
+}
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\charl\Documents\repos\LearnRust\guessing_game\target\debug\guessing_game.exe: C:\Users\charl\Documents\repos\LearnRust\guessing_game\src\main.rs

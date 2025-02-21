de29709ea194118879a1c94cf09d89ac8b7f6211
charlie-nickerson <charlie@nickerson.com>
2025-02-21 11:47:17 -0800

    Completed chapter 2 of the Rust Programming Language

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,31 +5,41 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess The Number");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new(); // Mutable
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");   
-
-    // Trim is necessary because when you hit ENTER while inputing
-    // a number it adds \r\n to the end of the string on windows
-    
-    // Parse allows for error handeling. If the parse method fails to convert
-    // the value into a number it will through an error
-    // Using the .expect method at the end will tell the user to make a valid input
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed {}", guess);
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
+    
+    loop {
+        println!("Please input your guess.");
+    
+        let mut guess = String::new(); // Mutable
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");   
 
-    println!("The secret number is {}", secret_number);
+        // Trim is necessary because when you hit ENTER while inputing
+        // a number it adds \r\n to the end of the string on windows
+        
+        // Parse allows for error handeling. If the parse method fails to convert
+        // the value into a number it will through an error
+        // Using the .expect method at the end will tell the user to make a valid input
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed {}", guess);
+
+        
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("The secret number is {}", secret_number);
+    }
 }

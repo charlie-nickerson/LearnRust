82a4b1ab5f95b888e9d3578271211e69f9571822
charlie-nickerson <charlie@nickerson.com>
2025-02-24 21:36:57 -0800

    Learned how to used derived traits for debugging structs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Debug)]
+struct Rect {
+    width: u32,
+    height: u32,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let scale = 2;
+    let rect1 = Rect {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+
+    println!("The area of the rectangle is {} square pixels",
+     area(&rect1));
+
+    println!("{rect1:#?}")
+}
+
+fn area(rect: &Rect) -> u32 {
+    rect.width * rect.height
 }

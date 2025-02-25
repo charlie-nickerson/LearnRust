505954af21eb3a10042a1c49007e86894a21bb13
charlie-nickerson <charlie@nickerson.com>
2025-02-24 20:46:33 -0800

    Started the rectangles program

---
+++ rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+C:\Users\charl\Documents\repos\LearnRust\rectangles\target\debug\deps\rectangles.exe: src\main.rs
+
+C:\Users\charl\Documents\repos\LearnRust\rectangles\target\debug\deps\rectangles.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Users\charl\Documents\repos\LearnRust\rectangles\target\debug\rectangles.exe: C:\Users\charl\Documents\repos\LearnRust\rectangles\src\main.rs

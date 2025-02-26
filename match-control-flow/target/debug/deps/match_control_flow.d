7dcda5fe1a33c469aff688aa9b32b067479bb2a2
charlie-nickerson <charlie@nickerson.com>
2025-02-26 15:30:06 -0800

    Started learning about match control flow

---
+++ match-control-flow/target/debug/deps/match_control_flow.d
@@ -0,0 +1,5 @@
+C:\Users\charl\Documents\Repositories\LearnRust\match-control-flow\target\debug\deps\match_control_flow.exe: src\main.rs
+
+C:\Users\charl\Documents\Repositories\LearnRust\match-control-flow\target\debug\deps\match_control_flow.d: src\main.rs
+
+src\main.rs:
---
+++ match-control-flow/target/debug/match-control-flow.d
@@ -0,0 +1,1 @@
+C:\Users\charl\Documents\Repositories\LearnRust\match-control-flow\target\debug\match-control-flow.exe: C:\Users\charl\Documents\Repositories\LearnRust\match-control-flow\src\main.rs

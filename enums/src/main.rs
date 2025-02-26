ddb25cf6fb26c38cfa806cc66c3253bc2fb4ce44
charlie-nickerson <charlie@nickerson.com>
2025-02-25 21:56:44 -0800

    Leanring enums

---
+++ enums/src/main.rs
@@ -0,0 +1,18 @@
+enum IpAddrKind{
+    V4(String),
+    V6(String),
+
+}
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+
+
+fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
+}
+
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+C:\Users\charl\Documents\repos\LearnRust\enums\target\debug\enums.exe: C:\Users\charl\Documents\repos\LearnRust\enums\src\main.rs

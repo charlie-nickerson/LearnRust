4a022e6624fb7211f298f4d1cb3e75cffae50100
charlie-nickerson <charlie@nickerson.com>
2025-02-25 23:29:18 -0800

    Learned about options

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,18 +1,9 @@
-enum IpAddrKind{
-    V4(String),
-    V6(String),
-
-}
-
-enum IpAddr {
-    V4(String),
-    V6(String),
-}
-
 
 
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
-    let loopback = IpAddr::V6(String::from("::1"));
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
 }
 

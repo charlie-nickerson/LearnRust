d1bf7c9520de10a1356995d483ba8ae773d25c92
charlie-nickerson <charlie@nickerson.com>
2025-02-28 15:37:29 -0800

    Learned about vectors and strings

---
+++ vectors/target/debug/deps/vectors.d
@@ -0,0 +1,5 @@
+C:\Users\charl\Documents\Repositories\LearnRust\vectors\target\debug\deps\vectors.exe: src\main.rs
+
+C:\Users\charl\Documents\Repositories\LearnRust\vectors\target\debug\deps\vectors.d: src\main.rs
+
+src\main.rs:
---
+++ vectors/target/debug/vectors.d
@@ -0,0 +1,1 @@
+C:\Users\charl\Documents\Repositories\LearnRust\vectors\target\debug\vectors.exe: C:\Users\charl\Documents\Repositories\LearnRust\vectors\src\main.rs

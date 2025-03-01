467daad6c8c0d0211d3e199ed82105355c1118e9
charlie-nickerson <charlie@nickerson.com>
2025-03-01 12:23:33 -0800

    Stored enums in vectors to have multiple different types in a vector

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,5 +1,13 @@
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
+    let v = vec![1, 2, 3, 4, 5];
 
     let third: &i32 = &v[3];
     println!("The third element is {third}");
@@ -9,5 +17,12 @@ fn main() {
         Some(third) => println!("The third element is {third}"),
         None => println!("There is no third element."),
     }
+
+    // Vectors can have multiple different types stored in them using enums
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(1.3),
+        SpreadsheetCell::Text(String::from("blue")),
+    ];
 
 }

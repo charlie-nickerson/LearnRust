03a27a8df8a7bff03d5aa07cd5263b0fc042b265
charlie-nickerson <charlie@nickerson.com>
2025-03-03 13:07:52 -0800

    Learned how to modify my hashmaps using .or_insert

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     use std::collections::HashMap;
 
@@ -19,5 +20,19 @@ fn main() {
         println!("{key}: {value}");
     }
 
+    let text = "Hello world hello wonderful world";
+
+    let mut map = HashMap::new();
+
+    // The split_whitespace method returns an iterator
+    // over subslices, separated by whitespace, of the value in text
+    // The or_insert method returns a mutable reference (&mut V) 
+    // to the value for the specified key.
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
 
 }

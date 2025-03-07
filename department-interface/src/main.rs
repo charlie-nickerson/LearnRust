43796e91396ceda9f8a2c5e4d3668c5a6bcf9c25
charlie-nickerson <charlie@nickerson.com>
2025-03-07 11:49:44 -0800

    Printed out the hashmap in ascending order based on the key values. This can be done by converting the hashmap into a vector of tuples. Instead of doing this I can use a Binary Tree instead.

--- department-interface/src/main.rs
+++ department-interface/src/main.rs
@@ -5,6 +5,7 @@ use std::io;
 // 2. User should be able to retrieve a list of all people in a department.
 // 3. User should be able to retrieve all users by department sorted alphametically.
 
+#[derive(Debug)]
 struct Department {
     name: String,
     employees: Vec<String>,
@@ -13,7 +14,7 @@ struct Department {
 
 
 fn initialize_departments() -> HashMap<usize, Department> {
-    let mut employees = HashMap::new();
+    let mut employees = HashMap::<usize, Department>::new();
 
     let departments = vec!["Human Resources",
     "Finance and Accounting",
@@ -36,16 +37,13 @@ fn initialize_departments() -> HashMap<usize, Department> {
 
         // Store the department in a hashmap
         employees.insert(id + 1, department);
-        
-        // Display department and ID
-        println!("{}: {name}", id + 1);
     }
 
     return employees
 
 }
 
-fn display_prompts() {
+fn add_employees_to_department(departments: &mut HashMap<usize, Department>) {
     let mut name: String = String::new();
     
     println!("Please type in a Name:");
@@ -58,10 +56,54 @@ fn display_prompts() {
     let name: String = name.trim().to_string();
 
     println!("Please select a department to add {name} to:");
-    let mut departments = initialize_departments();
 
+    // Create a vector to list out the departments by order of their key values
+    let mut entries: Vec<(&usize, &Department)>  = departments.iter().collect();
+    entries.sort_by_key(|entry|entry.0);
+    
+    // list all the departments
+    for (id, department) in entries {
+        println!("{id}: {:?}", department.name);
+    }
+
+
+    let mut choice =  String::new();
+
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+
+    let choice: usize = choice.trim().parse().expect("Please type a number!");
+
+    departments.get_mut(&choice).unwrap().employees.push(name);
+
+    println!("{:?}", departments);
 }
 
 fn main() {
-    display_prompts();
+    let mut departments = initialize_departments();
+    loop {
+        let mut choice =  String::new();
+        
+        println!("Select an option:
+        1. Add an employee
+        2. List employees from department
+        3. List all employees");
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+
+        let mut choice: usize = choice.trim().parse().expect("Please type a number!");
+    
+        if choice == 1 {
+            add_employees_to_department(&mut departments);
+        } else if choice == 2 {
+            todo!();
+        } else if choice == 3 {
+            todo!();
+        } else {
+            println!("Invalid option");
+        }
+    }
 }

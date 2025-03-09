9be5c12cc353ec7b3ba00b87f2c2afc2d4d9aeb3
charlie-nickerson <charlie@nickerson.com>
2025-03-09 15:34:45 -0700

    Learned how to use generic types in method implementations. Also learned how rust uses monomorphization to figure out the concrete types being used for the generic types during compilation. The compiler basically figures out the types that will be used to replace the generic types by looking at what variables types are passed into a functions parameters

---
+++ generic-types-2/target/debug/deps/generic_types_2.d
@@ -0,0 +1,5 @@
+C:\Users\charl\Documents\Repositories\LearnRust\generic-types-2\target\debug\deps\generic_types_2.exe: src\main.rs
+
+C:\Users\charl\Documents\Repositories\LearnRust\generic-types-2\target\debug\deps\generic_types_2.d: src\main.rs
+
+src\main.rs:
---
+++ generic-types-2/target/debug/generic-types-2.d
@@ -0,0 +1,1 @@
+C:\Users\charl\Documents\Repositories\LearnRust\generic-types-2\target\debug\generic-types-2.exe: C:\Users\charl\Documents\Repositories\LearnRust\generic-types-2\src\main.rs

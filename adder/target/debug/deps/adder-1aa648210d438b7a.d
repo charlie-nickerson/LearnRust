92c73d87860aebccfd9aac3233b75b72aa6d9380
charlie-nickerson <charlie@nickerson.com>
2025-03-10 10:52:30 -0700

    Learned how to write test functions

---
+++ adder/target/debug/deps/adder-1aa648210d438b7a.d
@@ -0,0 +1,7 @@
+/home/charlien/Documents/Repositories/LearnRust/adder/target/debug/deps/libadder-1aa648210d438b7a.rmeta: src/lib.rs
+
+/home/charlien/Documents/Repositories/LearnRust/adder/target/debug/deps/libadder-1aa648210d438b7a.rlib: src/lib.rs
+
+/home/charlien/Documents/Repositories/LearnRust/adder/target/debug/deps/adder-1aa648210d438b7a.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-89696b0864b9cc52.d
@@ -0,0 +1,5 @@
+/home/charlien/Documents/Repositories/LearnRust/adder/target/debug/deps/adder-89696b0864b9cc52: src/lib.rs
+
+/home/charlien/Documents/Repositories/LearnRust/adder/target/debug/deps/adder-89696b0864b9cc52.d: src/lib.rs
+
+src/lib.rs:

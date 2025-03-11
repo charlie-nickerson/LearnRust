aaf94f9d179e3fb84ee79a3bf4a6760e18c27e3e
charlie-nickerson <charlie@nickerson.com>
2025-03-11 15:29:43 -0700

    Read a file using fs and searched for the file using its path

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,22 @@
 use std::env;
+use std::fs;
 
 fn main() {
+
+    // .collect() can return many types of collections so we
+    // specify the args to be of type Vec<String>
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    // Must start at index 1 because the programs name takes up
+    // the first argument in args[0]
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("in file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read from file");
+
+    println!("Within text : \n{contents}");
+
 }

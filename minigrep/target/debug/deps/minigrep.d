4b4c31632edfc859b0e6fe74f03233f3bb2a69e4
charlie-nickerson <charlie@nickerson.com>
2025-03-11 15:05:34 -0700

    std::env allows you to use env::args().collect() to retrieve the command line arguments

---
+++ minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+C:\Users\charl\Documents\Repositories\LearnRust\minigrep\target\debug\deps\minigrep.exe: src\main.rs
+
+C:\Users\charl\Documents\Repositories\LearnRust\minigrep\target\debug\deps\minigrep.d: src\main.rs
+
+src\main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+C:\Users\charl\Documents\Repositories\LearnRust\minigrep\target\debug\minigrep.exe: C:\Users\charl\Documents\Repositories\LearnRust\minigrep\src\main.rs

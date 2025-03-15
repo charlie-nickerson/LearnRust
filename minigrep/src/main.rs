8f43a85b39d8e06ff2bbe98b9a4cb4a82275e7d7
charlie-nickerson <charlie@nickerson.com>
2025-03-15 14:05:43 -0700

    Added error handling for cases with too few argumenets in the build method

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
 
@@ -7,8 +8,12 @@ fn main() {
     // specify the args to be of type Vec<String>
     let args: Vec<String> = env::args().collect();
 
-    // Must start at index 1 because the programs name takes up
-    let config = parse_config(&args);
+    // We use build instead of new because programmers expect the new
+    // method to never throw an error
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read from file");
 
@@ -16,18 +21,30 @@ fn main() {
 
 }
 
+
+// Putting query and file_path under the same struct indicates
+// to programmers that these variables are related to each other
 struct Config {
     query: String,
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-        // the first argument in args[0]
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        println!("Searching for {query}");
-        println!("in file {file_path}");
-
-        Config {query, file_path}
+impl Config {
+    // Changing the parse_config function into a method for config
+    // shows the programmer that the function is strongly related
+    // to the variables in config. 
+    fn build(args: &[String]) -> Result<Config, &'static str>  {
+                // the first argument in args[0]
+                if args.len() < 3 {
+                    // Error methods always return string literals with 'static lifetime
+                    return Err("Too few arguments");
+                }
+                let query = args[1].clone();
+                let file_path = args[2].clone();
+            
+                println!("Searching for {query}");
+                println!("in file {file_path}");
+        
+                Ok(Config {query, file_path})
+    }
 }

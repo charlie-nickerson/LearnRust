e3ec4a091ca1db44c712418b9c1de1d4d9373711
charlie-nickerson <charlie@nickerson.com>
2025-03-16 17:11:31 -0700

    Learning about anonymous functions that capture their environment. This is an example of a closure

---
+++ shirt-color/target/debug/deps/shirt_color-ce926be6c39578d0.d
@@ -0,0 +1,5 @@
+/home/charlien/Documents/Repositories/LearnRust/shirt-color/target/debug/deps/shirt_color-ce926be6c39578d0: src/main.rs
+
+/home/charlien/Documents/Repositories/LearnRust/shirt-color/target/debug/deps/shirt_color-ce926be6c39578d0.d: src/main.rs
+
+src/main.rs:
---
+++ shirt-color/target/debug/shirt-color.d
@@ -0,0 +1,1 @@
+/home/charlien/Documents/Repositories/LearnRust/shirt-color/target/debug/shirt-color: /home/charlien/Documents/Repositories/LearnRust/shirt-color/src/main.rs

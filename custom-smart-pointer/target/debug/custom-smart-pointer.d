1f442f3bd58354c5e8d6ec6a4ed18306acfba531
charlie-nickerson <charlie@nickerson.com>
2025-03-22 15:12:27 -0700

    learned how to manually allocate and deallocate data by implementing the Drop trait

---
+++ custom-smart-pointer/target/debug/custom-smart-pointer.d
@@ -0,0 +1,1 @@
+C:\Users\charl\Documents\Repositories\LearnRust\custom-smart-pointer\target\debug\custom-smart-pointer.exe: C:\Users\charl\Documents\Repositories\LearnRust\custom-smart-pointer\src\main.rs
---
+++ custom-smart-pointer/target/debug/deps/custom_smart_pointer.d
@@ -0,0 +1,5 @@
+C:\Users\charl\Documents\Repositories\LearnRust\custom-smart-pointer\target\debug\deps\custom_smart_pointer.exe: src\main.rs
+
+C:\Users\charl\Documents\Repositories\LearnRust\custom-smart-pointer\target\debug\deps\custom_smart_pointer.d: src\main.rs
+
+src\main.rs:
